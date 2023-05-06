e24d501ac508bfa024ed2ce36d75133e76f42291
unknown <xSothe>
2023-05-07 01:18:53 +0200

    mostrando el problema

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,8 @@ public class Main {
         Empleado p2 = new Empleado("Pepe Pérez", -31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
 
+
+        System.out.println("Si quiero mostrar solo el nombre: " + p.nombre);
+        System.out.println("Si quiero mostrar solo la edad no puedo: " + p.edad); // necesito un getter
     }
 }
